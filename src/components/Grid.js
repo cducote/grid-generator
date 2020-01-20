fc2fb84f219d5b85bab5aed15c213751df8c4a8f
Chris Ducote <chrisducote@Chriss-MacBook-Air.local>
2020-01-20 11:06:49 +0100

    stuff is happening

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -10,12 +10,26 @@ class Grid extends Component {
        grid: {
            row: 1,
            col: 1
-       }
+       },
+       arr: [
+           {
+               id: 0,
+           },
+           {
+            id: 1,
+        },
+        {
+            id: 2,
+        },
+        {
+            id: 3,
+        },
+       ]
     }
 
-    componentDidMount = () => {
-     this.makeGrid()
-    }
+    // componentDidMount = () => {
+    //  this.makeGrid()
+    // }
 
     handleChange = async (e) => {
         let editGrid = { ...this.state.editGrid }
@@ -24,7 +38,7 @@ class Grid extends Component {
     }
 
     handleSubmit = async (e) => {
-        let grid = this.state.grid
+        // let grid = this.state.grid
         e.preventDefault()
         let editGrid = { ...this.state.grid }
         editGrid = {
@@ -34,25 +48,39 @@ class Grid extends Component {
         this.setState({
            grid: editGrid
         })
-        // Swal.fire(
-        //     'Good job!',
-        //     'Grid has been set!',
-        //     'success'
-        //   )
-        this.makeGrid(editGrid)
+        this.makeGrid()
     }
 
-    makeGrid = (grid) => {
-        for (var i=1; i <= this.state.grid.row; i++) {
-            console.log(this.state.grid.row)
-            return <tr></tr>
-        }
-        // for (var j=1; j <= grid.row; j++) {
-        //     return <td></td>
-        // }
+    makeGrid() {
+        let arr = this.state.arr
+        return arr.map((row, i) => {
+            return (
+            <tr key={i}>
+               {arr.map((col, j) =>{
+                   return (
+                       <td key={j}></td>
+                   )
+               })}
+            </tr>
+            )
+        })
     }
+    // makeGrid() {
+    //     let arr = this.state.arr
+    //     for (var i=0; i < arr.length; i++) {
+            
+    //         console.log(arr.length)
+    //         return(
+    //         <tr>
+    //             <td>
+    //             </td>
+    //         </tr>
+    //         )
+    //     }
+    // }
 
     render() {
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
