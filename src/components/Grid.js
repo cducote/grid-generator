81dbc2dbdac9cc9453eacdfb59099c862899bd76
Chris Ducote <chrisducote@Chriss-MacBook-Air.local>
2020-01-20 12:26:56 +0100

    grid generating complete

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,60 +3,37 @@ import React, { Component } from 'react';
 
 class Grid extends Component {
     state = {
-    //     editGrid:{
-    //         col: 0,
-    //         row: 0
-    //    },
-       grid: {
-           row: 1,
-           col: 1
-       },
-       arr: [
-           {
-               id: 0,
-           },
-           {
-            id: 1,
-        },
-        {
-            id: 2,
-        },
-        {
-            id: 3,
-        },
-       ],
-       userInput: 10,
+       userInput: 0,
        newRow: []
     }
     
 
-    componentDidMount = () => {
+    componentDidUpdate = async () => {
+        
+    }
+
+    handleChange = async ({ target: {value}}) => {
+       this.setState({ userInput: value })
+    }
+
+    handleSubmit = async (e) => {
+        // let grid = this.state.grid
+        e.preventDefault()
+        let input = parseInt(this.state.userInput, 10)
+        this.setState({
+           userInput: input
+        })
+        this.createObjects()
+        this.makeGrid()
+    }
+
+    createObjects(){
         const x = this.state.userInput
         let objects = []
         for (var i = 0; i < x; i++) {
             objects[i] = {id: i}
         } 
         this.setState({ newRow: objects })
-    }
-
-    handleChange = async (e) => {
-        let editGrid = { ...this.state.editGrid }
-        editGrid[e.target.name] = e.target.value
-        this.setState({ editGrid })
-    }
-
-    handleSubmit = async (e) => {
-        // let grid = this.state.grid
-        e.preventDefault()
-        let editGrid = { ...this.state.grid }
-        editGrid = {
-            row: 10,
-            col: 10
-        }
-        this.setState({
-           grid: editGrid
-        })
-        this.makeGrid()
     }
 
     makeGrid() {
@@ -75,13 +52,14 @@ class Grid extends Component {
     }
 
     render() {
-
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                Row<input onChange={this.handleChange} type='text' name='row' defaultValue='0'/>
-                Col<input onChange={this.handleChange} type='text' name='col' defaultValue='0' />
-                <button type='submit'>Submit</button>
+                Row <input onChange={this.handleChange} type='text' name='input' defaultValue='0' value={this.state.userInput}/> X
+                Col <input onChange={this.handleChange} type='text' name='input' defaultValue='0'  value={this.state.userInput} disabled/> <br/><br/><br/>
+                <div>
+                    <button type='submit'>Submit</button>
+                </div>
                 </form>
                 <br/>
                 <br/>
